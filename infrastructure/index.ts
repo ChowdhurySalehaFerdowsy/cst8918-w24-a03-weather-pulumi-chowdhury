2ffa961c2e242647bc235f602fa8b88c630e0dba
Akin Kuyga <kuyg0002@algonquinlive.com>
2024-01-29 22:56:29 -0500

    Read.ME file updated with screenshots

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -43,3 +43,88 @@ const registryCredentials = containerregistry
 
   export const acrServer = registry.loginServer
   export const acrUsername = registryCredentials.username
+
+// Other imports at the top of the module
+import * as docker from '@pulumi/docker'
+
+// ... rest of the code
+
+// Define the container image for the service.
+const image = new docker.Image(`${prefixName}-image`, {
+  imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+  build: {
+    context: appPath,
+    platform: 'linux/amd64'
+  },
+  registry: {
+    server: registry.loginServer,
+    username: registryCredentials.username,
+    password: registryCredentials.password
+  }
+})
+
+// Other imports at the top of the module
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
+
+// ... rest of the code
+
+// Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+  `${prefixName}-container-group`,
+  {
+    resourceGroupName: resourceGroup.name,
+    osType: 'linux',
+    restartPolicy: 'always',
+    imageRegistryCredentials: [
+      {
+        server: registry.loginServer,
+        username: registryCredentials.username,
+        password: registryCredentials.password
+      }
+    ],
+    containers: [
+      {
+        name: imageName,
+        image: image.imageName,
+        ports: [
+          {
+            port: containerPort,
+            protocol: 'tcp'
+          }
+        ],
+        environmentVariables: [
+          {
+            name: 'PORT',
+            value: containerPort.toString()
+          },
+          {
+            name: 'WEATHER_API_KEY',
+            value: '<your-secret-key>'
+          }
+        ],
+        resources: {
+          requests: {
+            cpu: cpu,
+            memoryInGB: memory
+          }
+        }
+      }
+    ],
+    ipAddress: {
+      type: containerinstance.ContainerGroupIpAddressType.Public,
+      dnsNameLabel: `${imageName}`,
+      ports: [
+        {
+          port: publicPort,
+          protocol: 'tcp'
+        }
+      ]
+    }
+  }
+)
+// Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply(addr => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply(addr => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+  addr => `http://${addr!.fqdn!}:${containerPort}`
+)
